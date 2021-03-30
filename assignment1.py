9953436ed38783157145727b44ed73f5d9aae24f
Daniel <daniel.benyaacov@gmail.com>
2021-03-30 18:25:19 +0300

    The math formula assignment 1.2

--- assignment1.py
+++ assignment1.py
@@ -1,3 +1,6 @@
+import random
+
+
 def reverse_func(input_func):
     """
     function decorator that reverses the target function’s positional
@@ -21,3 +24,27 @@ def sub(a, b):
     return a - b
 
 
+LEGAL_LETTERS = ["A", "B", "C"]
+
+
+def math_formula(input_formula: str, num_outputs: int):
+    """
+    The function receives as an input a string input_formula and num_outputs number.
+    The string input_formula includes a valid mathematical formula on variables named
+    A,B and C (example: s=’2*A+B/C’). The function selects n times random positive
+    integers A,B and C (in range 1-100) and prints the formula and its results.
+    :param input_formula: str
+    :param num_outputs: int
+    :return: None
+    """
+    original_input_formula = input_formula
+    for i in range(1, num_outputs):
+        input_formula = original_input_formula
+        for char in input_formula:
+            if char.isalpha():
+                if char in LEGAL_LETTERS:
+                    random_number = random.randint(1, 100)
+                    input_formula = input_formula.replace(char, str(random_number))
+                else:
+                    raise Exception("A non legal parameter input was given.\nThe legal parameters are: "+str(LEGAL_LETTERS))
+        print(str(input_formula), '=', eval(input_formula))
