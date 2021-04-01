1dffadbed7f69b6ef42dd21f14823b71e0552710
Daniel <daniel.benyaacov@gmail.com>
2021-04-01 15:36:34 +0300

    Support assigning types with cls_type

--- MagicList.py
+++ MagicList.py
@@ -2,15 +2,22 @@
 Magic List
 Create a Python class that implements a simplified list by skipping boundary checks when possible
 """
+from dataclasses import dataclass
+
+
+@dataclass
+class Person:
+    age: int = 1
 
 
 class WrappedList:
-    def __init__(self):
-        self._lst = [None]
+    def __init__(self, cls_type):
+        self.cls_type = cls_type
+        self._lst = [cls_type]
 
     def __getitem__(self, item):
         return self._lst[item]
 
     def __setitem__(self, key, value):
-        self._lst.append(None)
+        self._lst.append(self.cls_type)
         self._lst[key] = value
