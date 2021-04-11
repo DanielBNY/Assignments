1e743e65c125848d2e56e391178c8c842012c283
unknown <daniel.benyaacov@gmail.com>
2021-04-11 20:13:26 +0300

    File Scanner init

---
+++ DirScanner/Pattern.cs
@@ -0,0 +1,23 @@
+﻿namespace DirScanner
+{
+    public enum PatternType
+    {
+        FileName,
+        FileExtension
+    }
+
+    public class Pattern
+    {
+        public PatternType Type { get; }
+
+        public string Value { get; }
+
+        public Pattern(PatternType type, string value)
+        {
+            Type = type;
+            Value = value;
+        }
+    }
+
+
+}
---
+++ DirScanner/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+namespace DirScanner
+{
+    class Program
+    {
+        string PathToScan = @"C:\code\kela-interviews\Interviews\DirScanner\path_to_scan";
+
+        Pattern[] PatternsToMatch = new Pattern[] 
+        { 
+            new Pattern(PatternType.FileName, "doc1.txt"),
+            new Pattern(PatternType.FileName, "doc3.txt"),
+            new Pattern(PatternType.FileExtension, "log")
+        };
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
